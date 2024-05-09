f682a6881fb1b0a17b9508af167900932edbd33f
Aditya Pillai <107209683+adipill04@users.noreply.github.com>
2024-05-09 06:24:50 -0700

    Update training.sh

--- training/regularTraining/training.sh
+++ training/regularTraining/training.sh
@@ -1,40 +1,45 @@
-#script file to train dinov2 on a non-slurm environment (useful if trying to run on CHTC)
-
 #copy large files from /staging
-cp /staging/groups/caicedo_group/imagenet.zip ./
-cp /staging/groups/caicedo_group/extra.zip ./
+#cp /staging/groups/caicedo_group/imagenet.zip ./
+#cp /staging/groups/caicedo_group/extra.zip ./
 
 #extracting images from files
-unzip imagenet.zip &
-unzip extra.zip &
+#unzip imagenet.zip &
+#unzip extra.zip &
 
-wait
+#wait
 
 #setup train dir
-mkdir ./imagenet/train/train
-cd ./imagenet/train
-mv * train
-cd ..
-cd ..
+#mkdir ./imagenet/train/train
+#cd ./imagenet/train
+#mv * train
+#cd ..
+#cd ..
 
-#move config and modified train file
-mv config.yaml /home/dinov2/dinov2/configs/train
-mv train.py /home/dinov2/dinov2/train
+mv config.yaml /home/wandbIntegrate/dinov2/configs/train
+mv train.py /home/wandbIntegrate/dinov2/train
+mv __init__.py /home/wandbIntegrate/dinov2/fsdp
 
 #change to dinov2 dir
 export jobDir=$(pwd)
-cd /home/dinov2
+cd /home/wandbIntegrate
 mkdir output
 
-#init PYTHONPATH, CUDA
+#init PYTHONPATH
 export PYTHONPATH=.
-export CUDA_VISIBLE_DEVICES=0
+#export CUDA_VISIBLE_DEVICES=0
+export WANDB_API_KEY=61bf6d44e974b73170041842d514538f0ab4ed00
 
-#training script
-python -m torch.distributed.launch --nproc_per_node=1 ./dinov2/train/train.py --config-file=./dinov2/configs/train/config.yaml --output-dir=./output --no-resume train.dataset_path=ImageNet:split=TRAIN:root=$jobDir/imagenet/train:extra=$jobDir/extra & 
+#training script. initally starting with 1 node 1 epoch (torchrun --nproc_per_node=1)
+#torchrun --nproc_per_node=1 ./dinov2/run/train/train.py \
+ #   --nodes 1 \
+ #   --config-file dinov2/configs/train/config.yaml \
+ #   --output-dir ./output \
+ #   --no-resume \
+ #   train.dataset_path=ImageNet:split=TRAIN:root=$jobDir/imagenet/train:extra=$jobDir/extra &
 
+#python -m torch.distributed.launch
+torchrun --nproc_per_node=1 ./dinov2/train/train.py --config-file=./dinov2/configs/train/config.yaml --output-dir=./output train.dataset_path=ImageNet:split=TRAIN:root=/scratch/appillai/imagenet/train:extra=/scratch/appillai/extra &
 wait
 
-#copy output back to job directory
 tar -cvzf TrainingOutput.tar.gz output
 cp TrainingOutput.tar.gz $jobDir
