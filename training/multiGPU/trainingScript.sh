b454afc9760b4fb982179cff5532f6ca15b7971e
Aditya Pillai <107209683+adipill04@users.noreply.github.com>
2024-05-09 06:28:17 -0700

    Update and rename training.sh to trainingScript.sh

---
+++ training/multiGPU/trainingScript.sh
@@ -0,0 +1,36 @@
+#### script.sh #####
+#!/bin/sh
+
+# untar the test and training data
+#tar zxf MNIST_data.tar.gz
+
+if [ "$1" = "rank0" ]
+then
+      # rank0 port
+       h=$(hostname)
+   p=$2
+        echo "$h $p" > contact_file
+      python3 htchirp.py put contact_file rank0_contact
+      rm contact_file
+else
+      h=$1
+   p=$2
+fi
+
+python3 htchirp.py ulog "Running using $h:$p for the rendezvous"
+python3 htchirp.py set_job_attr C10dEndpoint \"$h:$p\"
+python3 htchirp.py set_job_attr ProvisionerState 2
+
+mv config.yaml /home/wandbIntegrate/dinov2/configs/train
+mv train.py /home/wandbIntegrate/dinov2/train
+mv __init__.py /home/wandbIntegrate/dinov2/fsdp
+
+#change to dinov2 dir
+export jobDir=$(pwd)
+cd /home/wandbIntegrate
+mkdir output
+
+export PYTHONPATH=.
+export WANDB_API_KEY=61bf6d44e974b73170041842d514538f0ab4ed00
+
+torchrun --nnodes 1:4 --nproc_per_node 2 --rdzv_backend c10d --rdzv-id 1 --rdzv-endpoint "$h:$p" ./dinov2/train/train.py --config-file=./dinov2/configs/train/config.yaml --output-dir=./output train.dataset_path=ImageNet:split=TRAIN:root=/scratch/appillai/imagenet/train:extra=/scratch/extra
