554efe969aa5ab6f330a70a8d3a84f7deb541ba1
Aditya Pillai <107209683+adipill04@users.noreply.github.com>
2024-05-09 06:29:02 -0700

    Update train.py

--- training/multiGPU/train.py
+++ training/multiGPU/train.py
@@ -1,274 +1,79 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-#
-# This source code is licensed under the Apache License, Version 2.0
-# found in the LICENSE file in the root directory of this source tree.
+Last login: Wed May  8 19:59:06 on console
 
-import wandb
+The default interactive shell is now zsh.
+To update your account to use zsh, please run `chsh -s /bin/zsh`.
+For more details, please visit https://support.apple.com/kb/HT208050.
+Adityas-MacBook-Pro-2:~ adityapillai$ ssh appillai@ap2002.chtc.wisc.edu
+(appillai@ap2002.chtc.wisc.edu) Password: 
+(appillai@ap2002.chtc.wisc.edu) Duo two-factor login for appillai
 
-import argparse
-import logging
-import math
-import os
-from functools import partial
+Enter a passcode or select one of the following options:
 
-from fvcore.common.checkpoint import PeriodicCheckpointer
-import torch
+ 1. Duo Push to XXX-XXX-6979
 
-from dinov2.data import SamplerType, make_data_loader, make_dataset
-from dinov2.data import collate_data_and_cast, DataAugmentationDINO, MaskingGenerator
-import dinov2.distributed as distributed
-from dinov2.fsdp import FSDPCheckpointer
-from dinov2.logging import MetricLogger
-from dinov2.utils.config import setup
-from dinov2.utils.utils import CosineScheduler
+Passcode or option (1-1): 1
+Success. Logging you in...
+Success. Logging you in...
+Last login: Wed May  8 21:35:59 2024 from 10.130.176.99
+_____________________________________________________________________
+ #####  #     # #######  #####  Issues?  Email chtc@cs.wisc.edu
+#     # #     #    #    #     # Unauthorized use prohibited by:
+#       #     #    #    #       WI Statutes: s. 947.0125
+#       #######    #    #       U.S. Code: 18 USC 1030
+#       #     #    #    #       U.S. Code: 18 USC 2510-2522
+#     # #     #    #    #     # U.S. Code: 18 USC 2701-2712
+ #####  #     #    #     #####  U.S. Code: 18 USC § 1831
+For off campus ssh access use https://www.doit.wisc.edu/network/vpn/
+_____________________________________________________________________
 
-from dinov2.train.ssl_meta_arch import SSLMetaArch
+           Virtual office hours are available twice a week:
+ Tuesdays, 10:30am - 12pm and Thursdays, 3:00 - 4:30pm (Central time)
+           Join via this link: go.wisc.edu/chtc-officehours
+        Sign in via this link: go.wisc.edu/chtc-officehours-signin
+Filesystem quota report
+Storage           Used (GB)    Limit (GB)    Files (#)    File Cap (#)    Quota (%)
+--------------  -----------  ------------  -----------  --------------  -----------
+/home/appillai         0.84            50         3073               0         1.67
 
-lrs = 0
-wds = 0
-momentums = 0
-teacher_temps = 0
+[appillai@ap2002 ~]$ cd dinoExperiments/
+[appillai@ap2002 dinoExperiments]$ ls
+trained
+[appillai@ap2002 dinoExperiments]$ cd trained/
+[appillai@ap2002 trained]$ ls
+config.yaml        datasetinstall.sub  gpu-chtc_1567674.log  gpu-chtc_1569705.log  gpu-chtc_1569884.log  gpu-chtc_1572552.log  gpu-chtc_1572988.log  __init__.py   training.sh   train.py
+datasetinstall.sh  docker_stderror     gpu-chtc_1569705.err  gpu-chtc_1569705.out  gpu-chtc_1569885.log  gpu-chtc_1572840.log  gpu-chtc_1573440.log  testmultigpu  training.sub
+[appillai@ap2002 trained]$ cd testmultigpu/
+[appillai@ap2002 testmultigpu]$ s
+-bash: s: command not found
+[appillai@ap2002 testmultigpu]$ ls
+config.yaml          docker_stderror  gpu.sub                 __init__.py         log        rank0_contact  train.dag.condor.sub  train.dag.dagman.out  train.dag.lib.out  train.dag.nodes.log  train.dag.rescue001.old  train.py
+debug-cli.25805.log  err.rank0        gpuworker.sub.template  injectRank0Name.sh  out.rank0  train.dag      train.dag.dagman.log  train.dag.lib.err     train.dag.metrics  train.dag.rescue001  trainingScript.sh
+[appillai@ap2002 testmultigpu]$ vim err.rank0
+[appillai@ap2002 testmultigpu]$ ls
+config.yaml          docker_stderror  gpu.sub                 __init__.py         log        rank0_contact  train.dag.condor.sub  train.dag.dagman.out  train.dag.lib.out  train.dag.nodes.log  train.dag.rescue001.old  train.py
+debug-cli.25805.log  err.rank0        gpuworker.sub.template  injectRank0Name.sh  out.rank0  train.dag      train.dag.dagman.log  train.dag.lib.err     train.dag.metrics  train.dag.rescue001  trainingScript.sh
+[appillai@ap2002 testmultigpu]$ cd ..
+[appillai@ap2002 trained]$ ls
+config.yaml        datasetinstall.sub  gpu-chtc_1567674.log  gpu-chtc_1569705.log  gpu-chtc_1569884.log  gpu-chtc_1572552.log  gpu-chtc_1572988.log  __init__.py   training.sh   train.py
+datasetinstall.sh  docker_stderror     gpu-chtc_1569705.err  gpu-chtc_1569705.out  gpu-chtc_1569885.log  gpu-chtc_1572840.log  gpu-chtc_1573440.log  testmultigpu  training.sub
+[appillai@ap2002 trained]$ vim train.py
+[appillai@ap2002 trained]$ vim training.sub
+[appillai@ap2002 trained]$ vim training.sh
+[appillai@ap2002 trained]$ vim config.yaml 
+[appillai@ap2002 trained]$ vim __init__.py 
+[appillai@ap2002 trained]$ vim training.sh
+[appillai@ap2002 trained]$ vim training.sub
+[appillai@ap2002 trained]$ vim training.sub
+[appillai@ap2002 trained]$ cd testmultigpu/
+[appillai@ap2002 testmultigpu]$ ls
+config.yaml          docker_stderror  gpu.sub                 __init__.py         log        rank0_contact  train.dag.condor.sub  train.dag.dagman.out  train.dag.lib.out  train.dag.nodes.log  train.dag.rescue001.old  train.py
+debug-cli.25805.log  err.rank0        gpuworker.sub.template  injectRank0Name.sh  out.rank0  train.dag      train.dag.dagman.log  train.dag.lib.err     train.dag.metrics  train.dag.rescue001  trainingScript.sh
+[appillai@ap2002 testmultigpu]$ vim __init
+[appillai@ap2002 testmultigpu]$ vim __init__.py 
+[appillai@ap2002 testmultigpu]$ vim config.yaml 
+[appillai@ap2002 testmultigpu]$ vim trainingScript.sh 
+[appillai@ap2002 testmultigpu]$ vim train.py
 
-torch.backends.cuda.matmul.allow_tf32 = True  # PyTorch 1.12 sets this to False by default
-logger = logging.getLogger("dinov2")
-
-def get_args_parser(add_help: bool = True):
-    parser = argparse.ArgumentParser("DINOv2 training", add_help=add_help)
-    parser.add_argument("--config-file", default="", metavar="FILE", help="path to config file")
-    parser.add_argument(
-        "--no-resume",
-        action="store_true",
-        help="Whether to not attempt to resume from the checkpoint directory. ",
-    )
-    parser.add_argument("--eval-only", action="store_true", help="perform evaluation only")
-    parser.add_argument("--eval", type=str, default="", help="Eval type to perform")
-    parser.add_argument(
-        "opts",
-        help="""
-Modify config options at the end of the command. For Yacs configs, use
-space-separated "PATH.KEY VALUE" pairs.
-For python-based LazyConfig, use "path.key=value".
-        """.strip(),
-        default=None,
-        nargs=argparse.REMAINDER,
-    )
-    parser.add_argument(
-        "--output-dir",
-        "--output_dir",
-        default="",
-        type=str,
-        help="Output directory to save logs and checkpoints",
-    )
-    parser.add_argument("--local-rank", default=0, type=int, help="Variable for distributed computing.")
-    return parser
-
-
-def build_optimizer(cfg, params_groups):
-    return torch.optim.AdamW(params_groups, betas=(cfg.optim.adamw_beta1, cfg.optim.adamw_beta2))
-
-
-def build_schedulers(cfg):
-    OFFICIAL_EPOCH_LENGTH = cfg.train.OFFICIAL_EPOCH_LENGTH
-    lr = dict(
-        base_value=cfg.optim["lr"],
-        final_value=cfg.optim["min_lr"],
-        total_iters=cfg.optim["epochs"] * OFFICIAL_EPOCH_LENGTH,
-        warmup_iters=cfg.optim["warmup_epochs"] * OFFICIAL_EPOCH_LENGTH,
-        start_warmup_value=0,
-    )
-    wd = dict(
-        base_value=cfg.optim["weight_decay"],
-        final_value=cfg.optim["weight_decay_end"],
-        total_iters=cfg.optim["epochs"] * OFFICIAL_EPOCH_LENGTH,
-    )
-    momentum = dict(
-        base_value=cfg.teacher["momentum_teacher"],
-        final_value=cfg.teacher["final_momentum_teacher"],
-        total_iters=cfg.optim["epochs"] * OFFICIAL_EPOCH_LENGTH,
-    )
-    teacher_temp = dict(
-        base_value=cfg.teacher["teacher_temp"],
-        final_value=cfg.teacher["teacher_temp"],
-        total_iters=cfg.teacher["warmup_teacher_temp_epochs"] * OFFICIAL_EPOCH_LENGTH,
-        warmup_iters=cfg.teacher["warmup_teacher_temp_epochs"] * OFFICIAL_EPOCH_LENGTH,
-        start_warmup_value=cfg.teacher["warmup_teacher_temp"],
-    )
-
-    #init global variables for wandb init
-    lrs = lr
-    wds = wd
-    momentums = momentum
-    teacher_temps = teacher_temp
-    
-    lr_schedule = CosineScheduler(**lr)
-    wd_schedule = CosineScheduler(**wd)
-    momentum_schedule = CosineScheduler(**momentum)
-    teacher_temp_schedule = CosineScheduler(**teacher_temp)
-    last_layer_lr_schedule = CosineScheduler(**lr)
-
-    last_layer_lr_schedule.schedule[
-        : cfg.optim["freeze_last_layer_epochs"] * OFFICIAL_EPOCH_LENGTH
-    ] = 0  # mimicking the original schedules
-
-    logger.info("Schedulers ready.")
-
-    return (
-        lr_schedule,
-        wd_schedule,
-        momentum_schedule,
-        teacher_temp_schedule,
-        last_layer_lr_schedule,
-    )
-
-
-def apply_optim_scheduler(optimizer, lr, wd, last_layer_lr):
-    for param_group in optimizer.param_groups:
-        is_last_layer = param_group["is_last_layer"]
-        lr_multiplier = param_group["lr_multiplier"]
-        wd_multiplier = param_group["wd_multiplier"]
-        param_group["weight_decay"] = wd * wd_multiplier
-        param_group["lr"] = (last_layer_lr if is_last_layer else lr) * lr_multiplier
-
-
-def do_test(cfg, model, iteration):
-    new_state_dict = model.teacher.state_dict()
-
-    if distributed.is_main_process():
-        iterstring = str(iteration)
-        eval_dir = os.path.join(cfg.train.output_dir, "eval", iterstring)
-        os.makedirs(eval_dir, exist_ok=True)
-        # save teacher checkpoint
-        teacher_ckp_path = os.path.join(eval_dir, "teacher_checkpoint.pth")
-        torch.save({"teacher": new_state_dict}, teacher_ckp_path)
-
-
-def do_train(cfg, model, resume=False):    
-    model.train()
-    inputs_dtype = torch.half
-    fp16_scaler = model.fp16_scaler  # for mixed precision training
-
-    # setup optimizer
-
-    optimizer = build_optimizer(cfg, model.get_params_groups())
-    (
-        lr_schedule,
-        wd_schedule,
-        momentum_schedule,
-        teacher_temp_schedule,
-        last_layer_lr_schedule,
-    ) = build_schedulers(cfg)
-
-    #init wandb
-    run = wandb.init(project="dinov2_training", config={"lr": lrs,"epochs": OFFICIAL_EPOCH_LENGTH,"wd": wds,"mom": momentums,"teacher_temp": teacher_temps,"batch_size_per_gpu" : 64},)
-
-    #watch model
-    wandb.watch(model)
-    
-    # checkpointer
-    checkpointer = FSDPCheckpointer(model, cfg.train.output_dir, optimizer=optimizer, save_to_disk=True)
-
-    start_iter = checkpointer.resume_or_load(cfg.MODEL.WEIGHTS, resume=resume).get("iteration", -1) + 1
-
-    OFFICIAL_EPOCH_LENGTH = cfg.train.OFFICIAL_EPOCH_LENGTH
-    max_iter = cfg.optim.epochs * OFFICIAL_EPOCH_LENGTH
-
-    periodic_checkpointer = PeriodicCheckpointer(
-        checkpointer,
-        period=3 * OFFICIAL_EPOCH_LENGTH,
-        max_iter=max_iter,
-        max_to_keep=3,
-    )
-
-    #init wandb
-    #run = wandb.init(project="testProject1", config={"lr": lr_schedule,"epochs": OFFICIAL_EPOCH_LENGTH,"wd": wd_schedule,"mom": momentum_schedule,"teacher_temp": teacher_temp_schedule,"batch_size_per_gpu" : 64},)
-
-    # setup data preprocessing
-
-    img_size = cfg.crops.global_crops_size
-    patch_size = cfg.student.patch_size
-    n_tokens = (img_size // patch_size) ** 2
-    mask_generator = MaskingGenerator(
-        input_size=(img_size // patch_size, img_size // patch_size),
-        max_num_patches=0.5 * img_size // patch_size * img_size // patch_size,
-    )
-
-    data_transform = DataAugmentationDINO(
-        cfg.crops.global_crops_scale,
-        cfg.crops.local_crops_scale,
-        cfg.crops.local_crops_number,
-        global_crops_size=cfg.crops.global_crops_size,
-        local_crops_size=cfg.crops.local_crops_size,
-    )
-
-    collate_fn = partial(
-        collate_data_and_cast,
-        mask_ratio_tuple=cfg.ibot.mask_ratio_min_max,
-        mask_probability=cfg.ibot.mask_sample_probability,
-        n_tokens=n_tokens,
-        mask_generator=mask_generator,
-        dtype=inputs_dtype,
-    )
-
-    # setup data loader
-
-    dataset = make_dataset(
-        dataset_str=cfg.train.dataset_path,
-        transform=data_transform,
-        target_transform=lambda _: (),
-    )
-    # sampler_type = SamplerType.INFINITE
-    sampler_type = SamplerType.SHARDED_INFINITE
-    data_loader = make_data_loader(
-        dataset=dataset,
-        batch_size=cfg.train.batch_size_per_gpu,
-        num_workers=cfg.train.num_workers,
-        shuffle=True,
-        seed=start_iter,  # TODO: Fix this -- cfg.train.seed
-        sampler_type=sampler_type,
-        sampler_advance=0,  # TODO(qas): fix this -- start_iter * cfg.train.batch_size_per_gpu,
-        drop_last=True,
-        collate_fn=collate_fn,
-    )
-
-    # training loop
-
-    iteration = start_iter
-
-    logger.info("Starting training from iteration {}".format(start_iter))
-    metrics_file = os.path.join(cfg.train.output_dir, "training_metrics.json")
-    metric_logger = MetricLogger(delimiter="  ", output_file=metrics_file)
-    header = "Training"
-
-    for data in metric_logger.log_every(
-        data_loader,
-        10,
-        header,
-        max_iter,
-        start_iter,
-    ):
-        current_batch_size = data["collated_global_crops"].shape[0] / 2
-        if iteration > max_iter:
-            return
-
-        # apply schedules
-
-        lr = lr_schedule[iteration]
-        wd = wd_schedule[iteration]
-        mom = momentum_schedule[iteration]
-        teacher_temp = teacher_temp_schedule[iteration]
-        last_layer_lr = last_layer_lr_schedule[iteration]
-        
-        apply_optim_scheduler(optimizer, lr, wd, last_layer_lr)
-
-        # compute losses
-
-        optimizer.zero_grad(set_to_none=True)
-        loss_dict = model.forward_backward(data, teacher_temp=teacher_temp)
-
-        # clip gradients
-
-        if fp16_scaler is not None:
-            if cfg.optim.clip_grad:
                 fp16_scaler.unscale_(optimizer)
                 for v in model.student.values():
                     v.clip_grad_norm_(cfg.optim.clip_grad)
@@ -295,8 +100,12 @@ def do_train(cfg, model, resume=False):
             logger.info("NaN detected")
             raise AssertionError
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+        #wandb logging
         wandb.log({"loss": losses_reduced})
         wandb.log({"learning rate": lr})
+
+
         metric_logger.update(lr=lr)
         metric_logger.update(wd=wd)
         metric_logger.update(mom=mom)
@@ -315,10 +124,10 @@ def do_train(cfg, model, resume=False):
     metric_logger.synchronize_between_processes()
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
-
+@record
 def main(args):
     #wandb.login()
-    
+
     cfg = setup(args)
 
     model = SSLMetaArch(cfg).to(torch.device("cuda"))
@@ -333,7 +142,7 @@ def main(args):
             + 1
         )
         return do_test(cfg, model, f"manual_{iteration}")
-    
+
     do_train(cfg, model, resume=not args.no_resume)
 
 
